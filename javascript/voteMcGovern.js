e9dcdf4209edf87c12f22fa1c3a9f0e3c00e4fdc
Jorge <jcollazo151@west-mec.org>
2021-04-16 15:23:35 -0700

    Formatted the index page and added confirms

---
+++ javascript/voteMcGovern.js
@@ -0,0 +1,36 @@
+let voteMcGovern = document.getElementById("voteMcGovern")
+
+voteMcGovern.onclick = function(){
+    if(confirm('are you sure')){
+        if(confirm('but are you really sure')){
+            if(confirm('but why')){
+                if(confirm('you know better')){
+                    if(confirm('there are two options but you choose this one')){
+                        if(confirm('We have free toast if you vote for Nixon')){
+                            if(confirm('Come on. Free toast!')){
+                                if(confirm('alright alright you win. press the red button to confirm your vote')){
+                                }else {
+                                    alert('smart choice')
+                                };
+                            }else {
+                                alert('smart choice')
+                            };
+                        }else {
+                            alert('smart choice')
+                        };
+                    }else {
+                        alert('smart choice')
+                    };
+                }else {
+                    alert('smart choice')
+                };
+            }else {
+                alert('smart choice')
+            };  
+        }else {
+            alert('smart choice')
+        };
+    } else {
+        alert('smart choice')
+    }
+};
