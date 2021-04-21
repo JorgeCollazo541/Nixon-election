894c7c4d690bd265790cb38c6cc5f96de11f117c
JorgeCollazo <jorgecollazojr541@gmail.com>
2021-04-21 00:17:35 -0700

    Fixed image scaling and attempted to add onload event

---
+++ javascript/general.js
@@ -0,0 +1,5 @@
+let home = document.getElementById("home")
+
+home.onload = function(){
+    alert('Vote today, your vote is important and we believe everyone should do it. As insentive to get people to vote, we are offering free toast')
+}
